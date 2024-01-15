d32ee9957a5bb809dba7b507873b1c9adb46a963
Eric <hayter.eric@gmail.com>
2024-01-14 22:15:39 -0500

    most of the util functions for change files are implemented
    must add update and pop functionality most likely done with tempfile python module

--- changes.py
+++ changes.py
@@ -15,6 +15,7 @@ operating system.
 
 import os
 import csv
+from logging import eprint
 
 
 CHANGE_FILE_NAME = 'changes.csv'
@@ -24,31 +25,44 @@ TIME_OF_MODIFICATION = 1
 
 def get_change_file_entry(change_file: str, entry_name: str):
     '''
-    returns an entry from the change file with the with corresponding 
+    returns an entry from the change file with the with corresponding
     entry name. Otherwise, returns None.
     '''
-    with open(change_file) as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=' ')
-        for row in csvreader:
-            if row[FILENAME] == str:
-                return row
-    return None
+    try:
+        with open(change_file) as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=' ')
+            for row in csvreader:
+                if row[FILENAME] == str:
+                    return row
+        return None
+    except FileNotFoundError:
+        eprint(f'[ERROR] file {change_file} not found.')
 
 
 def create_changes_file():
-    ''' creates a change file '''
+    ''' creates a change file to store most recent modification of files'''
     file = os.open(CHANGE_FILE_NAME)
     file.close()
 
 
 def change_file_add(change_file: str, entry_name: str, time: float):
-    with open(CHANGE_FILE_NAME):
-        # search if it's already in the csv file 
-        return 
+    if get_change_file_entry(change_file, entry_name) is not None:
+        eprint(f'[ERROR] entry {entry_name} is already in change file.')
+        return
+
+    with open(CHANGE_FILE_NAME) as csvfile:
+        csvwritter = csv.writer(csvfile, delimiter=' ')
+        csvwritter.writerow((entry_name, time))
 
 
 def change_file_update(change_file: str, entry_name: str, time: float):
+    '''
+    updates the time of an existing entry in the change file
+    '''
     if get_change_file_entry(change_file, entry_name) is None:
+        eprint(f'''[WARNING] entry {entry_name} does not exist in the change
+            file. An entry for {entry_name} has been created with the given
+            time {time}.''')
         change_file_add(change_file, entry_name, time)
         return
 
@@ -57,19 +71,22 @@ def change_file_update(change_file: str, entry_name: str, time: float):
 
 
 def change_file_pop():
-    return  
+    return
 
 
 def has_changed(change_file: str, entry_name: str):
     '''checks if a file has been changed'''
     # check if the file even exists
-    last_update_time = os.stat(entry_name).st_mtime
+    try:
+        last_update_time = os.stat(entry_name).st_mtime
+    except FileNotFoundError:
+        eprint(f'[ERROR] file {entry_name} not found.')
+        return
 
-    print(f'Last change was {last_update_time} seconds ago')
-
-    # get the stat of the object
-
-    # check the history file
-
-    # return if the dates of change are later than in the history file
-    return
+    last_recorded_modification = get_change_file_entry(change_file, entry_name)
+    if last_recorded_modification is None:
+        return True
+    elif last_recorded_modification[TIME_OF_MODIFICATION] == last_update_time:
+        return True
+    else:
+        return False
