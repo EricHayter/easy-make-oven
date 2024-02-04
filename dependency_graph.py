558e5846531f9950ae543de1b5bf2a848e4ecaa0
Eric <hayter.eric@gmail.com>
2024-02-04 11:59:46 -0500

    added graph traversal to check for cycles still need to test

--- dependency_graph.py
+++ dependency_graph.py
@@ -4,10 +4,21 @@ create a graph to map out the required dependencies
 
 
 class Node:
-    def __init__(self, file_name: str, dependencies: [str]):
+    def __init__(self, file_name: str, dependencies: ['Node']):
         self.file_name = file_name
         self.dependencies = dependencies
 
-    # this isn't good enough
-    def check_cycle(self, file: 'Node') -> bool:
-        return (file in self.dependencies) and (self in file.dependencies)
+    def get_dependencies(self):
+        return self.dependencies
+
+
+def check_cycle(start_node: Node, visited=[]):
+    if start_node in visited:  # already visited
+        return True
+    visited.append(start_node)
+    for node in start_node.get_dependencies():
+        if check_cycle(node, visited):
+            return True
+    return False
+
+# might want to add function to find islands
